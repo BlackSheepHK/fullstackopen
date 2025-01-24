60c08eff774bb040ba327f7bb9c6e585fcedb79b
KaLong Chan <kalongchan@KaLongs-MacBook-Pro.local>
2025-01-24 21:41:14 +0800

    Exercise 2.2

--- part2/courseinfo/components/Content.jsx
+++ part2/courseinfo/components/Content.jsx
@@ -5,6 +5,7 @@ const Content = ({parts}) => {
     return (
       <div>
         {parts.map(part => <Part part={part} key={part.id}/>)}
+        <p><b>total of {parts.map(part => part.exercises).reduce((a,b) => a+b)} exercises</b></p>
       </div>
     )
   }
