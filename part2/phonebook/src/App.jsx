e2a39a3031359ce22d2e271712e3b0a02464a5f1
KaLong Chan <kalongchan@KaLongs-MacBook-Pro.local>
2025-01-25 11:01:09 +0800

    Exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,25 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "/components/Filter";
 import FilteredRecords from "/components/FilteredRecords";
 import Form from "/components/Form";
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040-123456", id: 1 },
-		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
-		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-	]);
+	const [persons, setPersons] = useState([]);
 	const [filter, setFilter] = useState("");
+
+	const getInitialPersons = () => {
+		console.log("effect");
+		axios.get("http://localhost:3001/persons").then((response) => {
+			console.log("🚀 ~ axios.get ~ response:", response)
+			setPersons(response.data);
+		});
+	};
+	useEffect(getInitialPersons, [])
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
-      <Filter setFilter={setFilter}/>
+			<Filter setFilter={setFilter} />
 			<h2>add a new</h2>
 			<Form persons={persons} setPersons={setPersons} />
 			<h2>Numbers</h2>
-      <FilteredRecords persons={persons} filter={filter}/>
+			<FilteredRecords persons={persons} filter={filter} />
 		</div>
 	);
 };
