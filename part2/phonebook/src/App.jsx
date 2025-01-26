72d408750387799c43e62be712500462e1c5be84
KaLong Chan <kalongchan@KaLongs-MacBook-Pro.local>
2025-01-26 13:13:29 +0800

    Formatting

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,14 +3,14 @@ import Filter from "/components/Filter";
 import FilteredRecords from "/components/FilteredRecords";
 import Form from "/components/Form";
 import serverCommands from "/components/serverCommands";
-import ColorMessage from "/components/ColorMessage"
-import './index.css'
+import ColorMessage from "/components/ColorMessage";
+import "./index.css";
 
 const App = () => {
 	const [persons, setPersons] = useState([]);
 	const [filter, setFilter] = useState("");
-	const [topMessage, setTopMessage] = useState(null)
-	const [topMessageColor, setTopMessageColor] = useState(null)
+	const [topMessage, setTopMessage] = useState(null);
+	const [topMessageColor, setTopMessageColor] = useState(null);
 
 	const getInitialPersons = () => {
 		serverCommands.getPersons().then((response) => {
@@ -23,12 +23,18 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<ColorMessage message={topMessage} color={topMessageColor}/>
+			<ColorMessage message={topMessage} color={topMessageColor} />
 			<Filter setFilter={setFilter} />
 			<h2>add a new</h2>
 			<Form persons={persons} setPersons={setPersons} setTopMessage={setTopMessage} setTopMessageColor={setTopMessageColor} />
 			<h2>Numbers</h2>
-			<FilteredRecords persons={persons} setPersons={setPersons} filter={filter} setTopMessage={setTopMessage} setTopMessageColor={setTopMessageColor}/>
+			<FilteredRecords
+				persons={persons}
+				setPersons={setPersons}
+				filter={filter}
+				setTopMessage={setTopMessage}
+				setTopMessageColor={setTopMessageColor}
+			/>
 		</div>
 	);
 };
